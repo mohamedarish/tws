ee83ddf24d93625d674d2dca9d966b6c549e9260
mohamed arish <arish1248@gmail.com>
2024-02-02 23:41:38 +0530

    Now it searches for stuff and just takes all text, will need to make it only take required information

--- dist/content.js
+++ dist/content.js
@@ -1,13 +1,23 @@
 window.addEventListener("click", () => {
-	const elements = document.querySelectorAll("[data-testid=\"cellInnerDiv\"]");
+	const elements = document.querySelectorAll("[data-testid=\"cellInnerDiv\"] article");
 	console.log(elements);
 
 	let es = [];
 
 	for (let i = 0; i < elements.length; i += 1 ) {
-		es.push({
-			"content": elements[i].attributes.length
+		let reqElement = elements[i].querySelectorAll("span");
+
+		reqElement.forEach(el => {
+			es.push(
+				{
+					"something": el.innerText
+				}
+			)
 		})
+
+		// es.push({
+		// 	"content": elements[i].attributes.length
+		// })
 	}
 
 	browser.runtime.sendMessage({
